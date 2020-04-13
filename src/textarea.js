512d46f4187aea7829593b00992dfe4df0ddf2d9
Ed Clements <ed.clements@gmail.com>
2020-04-13 17:39:28 +0100

    Add textarea

---
+++ src/textarea.js
@@ -0,0 +1,29 @@
+import {SchemaFormField} from './field.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+<div class="form-group">
+    <label></label>
+    <textarea class="form-control"></textarea>
+    <small class="form-text text-muted"></small>
+    <div class="invalid-feedback"></div>
+</div>
+`;
+
+export class SchemaFormTextArea extends SchemaFormField {
+
+    connectedCallback() {
+        const node = document.importNode(template.content, true);
+        this.appendChild(node);
+        this.textareaElement = this.querySelector('textarea');
+        this.textareaElement.addEventListener('input', this.onChange.bind(this));
+    }
+
+    onChange(event) {
+        this.value = event.target.value;
+    }
+
+}
+
+window.customElements.define('schema-form-textarea', SchemaFormTextArea);
+
